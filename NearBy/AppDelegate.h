7037fb17c7e88e150674b4be1804db8d0940a489
henrywlu <henrywlu@yahoo.com>
2015-04-21 11:17:33 -0700

    Initial Commit

---
+++ NearBy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NearBy
+//
+//  Created by Henry W. Lu on 4/21/15.
+//  Copyright (c) 2015 Henry W. Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NearBy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NearBy
+//
+//  Created by Henry W. Lu on 4/21/15.
+//  Copyright (c) 2015 Henry W. Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
